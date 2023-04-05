2c5fa9f44116a4f86e5fa21efa5995a4e48aae4f
R5\Andrey <andrej.shmalenko@gmail.com>
2023-04-05 23:38:43 +0300

    Add pages: Home, Posts, Register, Login.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import {Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/Home';
+import Posts from './pages/Posts';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
 function App() {
-  return <div className="App">Hello there!</div>;
+    return (
+        <>
+            <header>
+                <Link to = "/">Home</Link>
+                <br/><br/>
+                <Link to = "/posts">Posts</Link>
+                <br/><br/>
+                <Link to = "/register">Register</Link>
+                <br/><br/>
+                <Link to = "/login">Login</Link>
+                <br/><br/>
+            </header>
+
+            <Routes>
+                <Route path='/' element={<Home/>}></Route>
+                <Route path='/posts' element={<Posts/>}></Route>
+                <Route path='/register' element={<Register/>}></Route>
+                <Route path='/login' element={<Login/>}></Route>
+
+            </Routes>
+        </>
+
+    );
 }
 
 export default App;
