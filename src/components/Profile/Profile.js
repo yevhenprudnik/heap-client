36135a6ea374fee3e039fd1bb32710bd0f0a22ce
GLFV <okolesnik753@gmail.com>
2023-05-31 15:04:55 +0300

    Profile and posts pages connected to api.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,28 +1,28 @@
 import '../../App.css';
 import './Profile.css';
 
-export default function Profile({userExample}) {
+export default function Profile({ user }) {
   return (
-    <div className='flex-ns flex-column items-center'>
-      <div className='flex-ns flex-column items-center w-50 bg-light-gray'>
-        <div className='flex-ns'>
-          <div className='flex-ns justify-center items-center w50p'>
-            <div className='avatar'>
-              <img src={userExample.avatar} alt='' className='avatar-image' />
+    <div className="flex-ns flex-column items-center">
+      <div className="flex-ns flex-column items-center w-50 bg-light-gray">
+        <div className="flex-ns">
+          <div className="flex-ns justify-center items-center w50p">
+            <div className="avatar">
+              <img src={user.avatar} alt="" className="avatar-image" />
             </div>
           </div>
-          <div className='flex-ns flex-column'>
-            <div className='fw6 f1'>{userExample.username}</div>
-            <div className='flex-ns justify-around f3 tc mv1'>
-              <div className='pa1'>7 publications</div>
-              <div className='pa1'>100 subscribers</div>
-              <div className='pa1'>13 subscriptions</div>
+          <div className="flex-ns flex-column">
+            <div className="fw6 f1">{user.username}</div>
+            <div className="flex-ns justify-around f3 tc mv1">
+              <div className="pa1">7 publications</div>
+              <div className="pa1">100 subscribers</div>
+              <div className="pa1">13 subscriptions</div>
             </div>
-            <div className='f4 mt4'>{userExample.desription}</div>
+            {/* <div className="f4 mt4">{user.desription}</div> */}
           </div>
         </div>
       </div>
-      <div className='flex-ns justify-around w-50 mv3 pv3 bg-light-gray'>
+      <div className="flex-ns justify-around w-50 mv3 pv3 bg-light-gray">
         <button>New post</button>
         <button>New story</button>
         <button>Settings</button>
