b93db28362db7c751c9adfc7a3f436a8b7e3f98b
GLFV <okolesnik753@gmail.com>
2023-06-07 11:12:56 +0300

    Fix profile setting

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,14 +1,10 @@
 export default function HomePage() {
   return (
-    <div className='HomePage flex-ns justify-center'>
-      <div className='mt7 flex-ns f2 fw6 black w-70 justify-center'>
+    <div className="HomePage flex-ns justify-center">
+      <div className="mt7 flex-ns f2 fw6 black w-70 justify-center">
         This social network was created by four friends from Uzhgorod National
-        University (Yevhen Prudnik, Vasyl Sinko, Oleksandr Kolesnyk and Andriy
-        Shmalenko). To test the functionality of the site, please log in to an
-        existing account or create a new one if you do not have one. Of course,
-        there may be bugs and shortcomings on the site, since this is the first
-        major project of the creators. Please don't judge harshly, and welcome
-        :)
+        University. To test the functionality of the site, please log in to an
+        existing account or create a new if you do not have one :)
       </div>
     </div>
   );
